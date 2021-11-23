fe1efae57e8465a5e5bb7992521dbfde52491182
andrewroddy92 <andrewroddy92@yahoo.com>
2021-11-23 09:50:18 -0500

    wip resolver mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -44,44 +44,31 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    /*
-    // Add a third argument to the resolver to access data in our `context`
-    addSkill: async (parent, { profileId, skill }, context) => {
-      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
-      if (context.user) {
-        return Profile.findOneAndUpdate(
-          { _id: profileId },
-          {
-            $addToSet: { skills: skill },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      // If user attempts to execute this mutation and isn't logged in, throw an error
-      throw new AuthenticationError('You need to be logged in!');
+    saveBook: async (parent, { user, book }, context) => {
+        if(context.user) {
+            return User.findOneAndUpdate(
+                { _id: user._id },
+                {
+                  $addToSet: { savedBooks: book },
+                },
+                {
+                  new: true,
+                  runValidators: true,
+                }
+              );
+        }
+        throw new AuthenticationError('You need to be logged in!');
     },
-    // Set up mutation so a logged in user can only remove their profile and no one else's
-    removeProfile: async (parent, args, context) => {
-      if (context.user) {
-        return Profile.findOneAndDelete({ _id: context.user._id });
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    // Make it so a logged in user can only remove a skill from their own profile
-    removeSkill: async (parent, { skill }, context) => {
-      if (context.user) {
-        return Profile.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { skills: skill } },
-          { new: true }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    */
+    removeBook: async (parent, { user, bookId }, context) => {
+        if(context.user) {
+            return User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+        }
+        throw new AuthenticationError('You need to be logged in!');
+    }
   },
 };
 
